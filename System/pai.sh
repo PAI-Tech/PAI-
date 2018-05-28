1abb153e2a5c2668a5a863652e13cad19d7af448
Captain Crypto <tamir@crypto-knight.tech>
2018-05-28 17:53:14 +0300

    PAI-OS System scripts

---
+++ System/pai.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+	
+#	Created by Eran Caballero on 03/05/2018.	
+#	Copyrights PAI Tech 2018, all rights reserved 
+
+
+PAI=/var/PAI
+PAI_SYSTEM_FOLDER=$PAI/System
+
+
+. $PAI_SYSTEM_FOLDER/pai-os.sh
+. $PAI_SYSTEM_FOLDER/pai-log.sh
