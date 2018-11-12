702b18e549682e0b8fdaa837ef6f039744f84637
pai-git <git@pai-tech.org>
2018-11-12 16:29:01 +0000

    bot install

---
+++ Install/pai-log.sh
@@ -0,0 +1,38 @@
+
+#!/bin/bash
+
+#
+#	PAI Logger
+#	Write to log file.
+# Created by Eran Caballero on 03/05/2018.
+# Copyrights PAI Tech 2018, all rights reserved
+
+
+PAI_OS_LOG_FILE=$PAI/Logs/pai.log
+
+pai_file_log()
+{
+	NOW=$(date)
+	echo " | $1" >>$PAI_OS_LOG_FILE
+}
+
+
+pai_log_console()
+{
+	echo "  PAI: $1"
+}
+
+pai_log()
+{
+	NOW=$(date +%F-%T)
+	pai_log_console "[$NOW] $1"
+	pai_file_log "[$NOW] $1"
+}
+
+pai_log_sep()
+{
+	_SEP="+----------------------------------------------------------------------"
+	echo ""
+	echo $_SEP
+	echo ""
+}
---
+++ Install/pai.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+#	Created by Eran Caballero on 03/05/2018.
+#	Copyrights PAI Tech 2018, all rights reserved
+
+
+PAI=/var/PAI
+#PAI_SYSTEM_FOLDER=$PAI/System
+
+
+. ./pai-os.sh
+. ./pai-log.sh
